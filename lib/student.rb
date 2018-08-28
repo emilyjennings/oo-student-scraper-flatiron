548f61d3502785a60924d132f2c9bd18a3b09e3b
Emily Jennings <emily.jennings@flatironschool.com>
2018-08-28 23:01:56 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -16,9 +16,7 @@ class Student
   def self.create_from_collection(students_array)
     students_array.each do |student|
       #binding.pry
-      new_student = {}
-      student[:name] = @name
-      student[:location] = @location
+      student = self.new(student)
     end
 
   end
