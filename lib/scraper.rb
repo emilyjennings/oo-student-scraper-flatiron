b019be9561232b1878ad6236e058b05cee82ebba
Emily Jennings <emily.jennings@flatironschool.com>
2018-08-28 23:10:22 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,10 +3,11 @@ require 'pry'
 require 'nokogiri'
 
 class Scraper
+  attr_accessor :student
   @@all = []
 
   def self.scrape_index_page(index_url)
-    html = open(index_url) # does File.read() give anything different?
+    html = open(index_url)
 
     page = Nokogiri::HTML(html)
 
--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,6 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url, :scraper
 
   @@all = []
 
@@ -8,20 +8,29 @@ class Student
     #binding.pry
     @name = student_hash[:name]
     @location = student_hash[:location]
-    student = student_hash
-    @@all << student
+    student_hash = self
+    @@all << self
 
   end
 
   def self.create_from_collection(students_array)
+    students_array.each do |student|
+      student = self.new(student)
+    end
 
   end
 
   def add_student_attributes(attributes_hash)
-
+    #binding.pry
+    @twitter = attributes_hash[:twitter]
+    @linkedin = attributes_hash[:linkedin]
+    @github = attributes_hash[:github]
+    @blog = attributes_hash[:blog]
+    @profile_quote = attributes_hash[:profile_quote]
+    @bio = attributes_hash[:bio]
   end
 
   def self.all
-
+    @@all
   end
 end
