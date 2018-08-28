a976495df66ad219e83ec254b3f672cd326f89ca
Emily Jennings <emily.jennings@flatironschool.com>
2018-08-28 23:09:39 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -26,6 +26,7 @@ class Student
     @linkedin = attributes_hash[:linkedin]
     @github = attributes_hash[:github]
     @blog = attributes_hash[:blog]
+    @profile_quote = attributes_hash[:profile_quote]
   end
 
   def self.all
